2d69380fc701326f82fc9f24b52266cb767c478e
seanaleid <seanaleid@gmail.com>
2020-11-04 00:51:11 -0500

    Add recommendations

--- src/components/about.js
+++ src/components/about.js
@@ -2,6 +2,10 @@ import React from "react";
 import Container from "@material-ui/core/Container";
 import { makeStyles } from "@material-ui/core/styles";
 import { Typography } from "@material-ui/core";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles({
   box: {
@@ -20,6 +24,18 @@ const useStyles = makeStyles({
   },
   text: {
     marginBottom: "1rem",
+  },
+  cardContainer: {
+    display: "flex",
+    flexWrap: "wrap",
+    paddingBottom: "50px",
+  },
+  cardContent: {
+    background: "#FAFAFA",
+  },
+  card: {
+    width: "260px",
+    margin: "20px",
   },
 });
 
@@ -60,6 +76,144 @@ const About = () => {
         <Typography variant="h2" className={classes.title}>
           Recommendations
         </Typography>
+        <Container className={classes.cardContainer}>
+          <Card className={classes.card}>
+            <CardContent className={classes.cardContent}>
+              <Typography variant="h5" component="h2">
+                Erik R.
+              </Typography>
+              <Typography className={classes.pos} color="textSecondary">
+                I was fortunate to be placed under Sean's leadership at Lambda.
+                Sean is a phenomenal person, speaker, and educator! One of his
+                skills I admired while interacting with Sean is his ability to
+                explain and break down complex software problems...
+              </Typography>
+            </CardContent>
+            <CardActions className={classes.cardContent}>
+              <Button
+                size="small"
+                href="https://www.linkedin.com/in/sean-naleid/"
+                target="_blank"
+              >
+                Read More
+              </Button>
+            </CardActions>
+          </Card>
+          <Card className={classes.card}>
+            <CardContent className={classes.cardContent}>
+              <Typography variant="h5" component="h2">
+                Josue R.
+              </Typography>
+              <Typography className={classes.pos} color="textSecondary">
+                Sean was my Team Leader in Lambda and let me just say that Sean
+                is extremely capable and regularly goes beyond his role
+                description to get the job done. I’ve worked under Sean for well
+                over 3 months and has provided not only myself, but our whole
+                team...
+              </Typography>
+            </CardContent>
+            <CardActions className={classes.cardContent}>
+              <Button
+                size="small"
+                href="https://www.linkedin.com/in/sean-naleid/"
+                target="_blank"
+              >
+                Read More
+              </Button>
+            </CardActions>
+          </Card>
+          <Card className={classes.card}>
+            <CardContent className={classes.cardContent}>
+              <Typography variant="h5" component="h2">
+                Luis P.
+              </Typography>
+              <Typography className={classes.pos} color="textSecondary">
+                Sean was my TL during the CORE Curriculum of Lambda School. At
+                all times, Sean demonstrated professionalism and full mastery
+                over the course material. Sean always went above and beyond to
+                help his students understand the material beyond...
+              </Typography>
+            </CardContent>
+            <CardActions className={classes.cardContent}>
+              <Button
+                size="small"
+                href="https://www.linkedin.com/in/sean-naleid/"
+                target="_blank"
+              >
+                Read More
+              </Button>
+            </CardActions>
+          </Card>
+          <Card className={classes.card}>
+            <CardContent className={classes.cardContent}>
+              <Typography variant="h5" component="h2">
+                William R.
+              </Typography>
+              <Typography className={classes.pos} color="textSecondary">
+                If I had to sum Sean up in one word it would most definitely be
+                Teacher. In it's truest form Sean was not simply a team leader
+                but I would go as far as calling him our personal instructor,
+                motivator, and most of all friend. He would take any and all
+                time needed to not only help us solve...
+              </Typography>
+            </CardContent>
+            <CardActions className={classes.cardContent}>
+              <Button
+                size="small"
+                href="https://www.linkedin.com/in/sean-naleid/"
+                target="_blank"
+              >
+                Read More
+              </Button>
+            </CardActions>
+          </Card>
+          <Card className={classes.card}>
+            <CardContent className={classes.cardContent}>
+              <Typography variant="h5" component="h2">
+                Carl S.
+              </Typography>
+              <Typography className={classes.pos} color="textSecondary">
+                Throughout my time at Lambda, Sean was the person I turned
+                everything into and asked all my questions. Sean not only spent
+                hours and hours of extra time with every single one of his
+                students in his group to ensure success, but also created a
+                comfortable and productive work environment...
+              </Typography>
+            </CardContent>
+            <CardActions className={classes.cardContent}>
+              <Button
+                size="small"
+                href="https://www.linkedin.com/in/sean-naleid/"
+                target="_blank"
+              >
+                Read More
+              </Button>
+            </CardActions>
+          </Card>
+          <Card className={classes.card}>
+            <CardContent className={classes.cardContent}>
+              <Typography variant="h5" component="h2">
+                Brandon H.
+              </Typography>
+              <Typography className={classes.pos} color="textSecondary">
+                Sean is one of the best people I have had the pleasure of
+                working with. During my time as Section Lead I witnessed many
+                students and fellow Team Leads receive help, either with advice
+                on how to handle a situation, or some technical part of coding,
+                and come away...
+              </Typography>
+            </CardContent>
+            <CardActions className={classes.cardContent}>
+              <Button
+                size="small"
+                href="https://www.linkedin.com/in/sean-naleid/"
+                target="_blank"
+              >
+                Read More
+              </Button>
+            </CardActions>
+          </Card>
+        </Container>
       </Container>
     </React.Fragment>
   );
